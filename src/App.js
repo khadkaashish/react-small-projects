b1ddb10a71b51a5e2b69b3a8602623e0b99e761d
khadkaashish <ashishkhadka03@gmail.com>
2023-10-02 14:40:25 +0545

    header_menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <h1>Hello World!!!</h1>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-  return <div></div>;
+  return (
+    <div classname="header">
+      <div className="header__options">
+        <div className="header__optionsTitle">Title 1</div>
+        <div className="header__optionsTitle">Title 2</div>
+        <div className="header__optionsTitle">Title 3</div>
+        <div className="header__optionsTitle">Title 4</div>
+        <div className="header__optionsTitle">Title 5</div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
